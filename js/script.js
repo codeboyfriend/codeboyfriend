cfbc03a1ff534d736512cfd1a69187537a6477a0
codeboyfriend <mkinggrafix@gmail.com>
2022-06-26 10:48:12 -0700

    initial

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let homeBtn = document.querySelector('.home'),
+let btn = document.querySelector('.btn'),
+    home = document.querySelector('.home')
     modal = document.querySelector('.modal'),
     slide = document.querySelector('.slide'),
     msg = document.querySelector('.msg'),
@@ -7,27 +8,29 @@ let homeBtn = document.querySelector('.home'),
     twitter = document.querySelector('.twitter'),
     watsapp = document.querySelector('.watsapp');
 
-homeBtn.addEventListener('click', () => {
-    homeBtn.classList.toggle('active');
+btn.addEventListener('click', () => {
     toggleBtn();
-    if (homeBtn.classList.contains('active')) {
-        modal.style.transform = 'translateX(-300px)';
-        slide.style.transform = 'translateX(0px)';
-        msg.style.opacity = 1;
-        homeBtn.classList.add('fa-xmark');
-        homeBtn.classList.remove('fa-circle-dot')
+    msg.classList.toggle('active')
+    if(msg.classList.contains('active')) {
+        modal.style.display = 'none';
+        slide.style.display = 'block';
+        home.classList.add('fa-xmark');
+        home.classList.remove('fa-circle-dot')
+        btn.style.backgroundColor = '#bb93fc';
+        btn.style.color = '#fff';
     }else {
-        modal.style.transform = 'translateX(0px)';
-        slide.style.transform = 'translateX(1400px)';
-        msg.style.opacity = 0;
-        homeBtn.classList.remove('fa-xmark');
-        homeBtn.classList.add('fa-circle-dot')
+        modal.style.display = 'block';
+        slide.style.display = 'none';
+        btn.style.backgroundColor = '#fff';
+        btn.style.color = '#151515';
+        home.classList.remove('fa-xmark');
+        home.classList.add('fa-circle-dot')
     }
-})
+});
 
 function toggleBtn() {
     instagram.classList.toggle('left');
     github.classList.toggle('top');
     twitter.classList.toggle('right');
     watsapp.classList.toggle('bottom');
-}
+};

1c68d7b12ff8c307bf9bcace293ba92373aafea3
codeboyfriend <mkinggrafix@gmail.com>
2022-06-27 00:23:17 -0700

    initial

--- js/script.js
+++ js/script.js
@@ -1,36 +1,18 @@
-let btn = document.querySelector('.btn'),
-    home = document.querySelector('.home'),
-    modal = document.querySelector('.modal'),
-    slide = document.querySelector('.slide'),
-    msg = document.querySelector('.msg'),
-    instagram = document.querySelector('.instagram'),
-    github = document.querySelector('.github'),
-    twitter = document.querySelector('.twitter'),
-    watsapp = document.querySelector('.watsapp');
+let openBtn = document.getElementById('openBtn'),
+    closeBtnOne = document.getElementById('closeBtnOne'),
+    closeBtnTwo = document.getElementById('closeBtnTwo'),
+    leftSlide = document.querySelector('.left_slide'),
+    rightSlide = document.querySelector('.right_slide');
 
-btn.addEventListener('click', () => {
-    toggleBtn();
-    msg.classList.toggle('active')
-    if(msg.classList.contains('active')) {
-        modal.style.display = 'none';
-        slide.style.display = 'block';
-        home.classList.add('fa-xmark');
-        home.classList.remove('fa-circle-dot')
-        btn.style.backgroundColor = '#bb93fc';
-        btn.style.color = '#fff';
-    }else {
-        modal.style.display = 'block';
-        slide.style.display = 'none';
-        btn.style.backgroundColor = '#fff';
-        btn.style.color = '#151515';
-        home.classList.remove('fa-xmark');
-        home.classList.add('fa-circle-dot')
-    }
-});
+openBtn.addEventListener('click', () =>{
+    leftSlide.style.width = '50%';
+    rightSlide.style.width = '50%';
+})
 
-function toggleBtn() {
-    instagram.classList.toggle('left');
-    github.classList.toggle('top');
-    twitter.classList.toggle('right');
-    watsapp.classList.toggle('bottom');
+closeBtnOne.addEventListener('click', closeSlide);
+closeBtnTwo.addEventListener('click', closeSlide)
+
+function closeSlide() {
+    leftSlide.style.width = '0%';
+    rightSlide.style.width = '0%';
 };
